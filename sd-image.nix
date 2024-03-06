8423695dfaf7a85a88e820e98d4a2d7e1db84f12
PKuppili <44453209+PKuppili@users.noreply.github.com>
2024-03-05 21:33:10 -0500

    Added Pranav's Public Key

--- sd-image.nix
+++ sd-image.nix
@@ -18,6 +18,7 @@
   users.extraUsers.nixos.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSt9Z8Qdq068xj/ILVAMqmkVyUvKCSTsdaoehEZWRut rcmast3r1@gmail.com"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPhMu3LzyGPjh0WkqV7kZYwA+Hyd2Bfc+1XQJ88HeU4A rcmast3r1@gmail.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZRFnx0tlpUAFqnEqP2R/1y8oIAPXhL2vW/UU727vw8 eddsa-key-Pranav"
   ];
 
   # bzip2 compression takes loads of time with emulation, skip it. Enable this if you're low
