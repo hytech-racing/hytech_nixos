5928814e69de607051f7b0e24049b76029821fb2
Ben Hall <rcmast3r1@gmail.com>
2024-03-20 20:54:16 -0400

    working linux router

--- flake.nix
+++ flake.nix
@@ -8,16 +8,32 @@
   };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/8bf65f17d8070a0a490daf5f1c784b87ee73982c";
-    hytech_data_acq.url = "github:RCMast3r/data_acq";
+    hytech_data_acq.url = "github:RCMast3r/data_acq/2024-03-18T20_52_15";
+    hytech_data_acq.inputs.ht_can_pkg_flake.url = "github:hytech-racing/ht_can/31";
     raspberry-pi-nix.url = "github:tstat/raspberry-pi-nix";
 
   };
   outputs = { self, nixpkgs, hytech_data_acq, raspberry-pi-nix }: rec {
 
-    shared_config = {
-      nixpkgs.overlays = [ (hytech_data_acq.overlays.default) ];
 
-      # nixpkgs.config.allowUnsupportedSystem = true;
+    user_setup = { pkgs, lib, ... }:
+      {
+        nix.settings.require-sigs = false;
+        users.users.nixos.group = "nixos";
+        users.users.root.initialPassword = "root";
+        users.users.nixos.password = "nixos";
+        users.users.nixos.extraGroups = [ "wheel" ];
+        users.groups.nixos = { };
+        users.users.nixos.isNormalUser = true;
+
+        system.activationScripts.createRecordingsDir = lib.stringAfter [ "users" ] ''
+          mkdir -p /home/nixos/recordings
+          chown nixos:users /home/nixos/recordings
+        '';
+      };
+    shared_config = {
+      nixpkgs.overlays = hytech_data_acq.overlays.aarch64-linux;
+      services.automatic-timezoned.enable = true;
       nixpkgs.hostPlatform.system = "aarch64-linux";
 
       systemd.services.sshd.wantedBy =
@@ -45,7 +61,7 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPhMu3LzyGPjh0WkqV7kZYwA+Hyd2Bfc+1XQJ88HeU4A rcmast3r1@gmail.com"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZRFnx0tlpUAFqnEqP2R/1y8oIAPXhL2vW/UU727vw8 eddsa-key-Pranav"
       ];
-      networking.useDHCP = false;
+      networking.useDHCP = true;
       # users.extraUsers.nixos.openssh.extraConfig = "AddressFamily = any";
       networking.hostName = "hytech-pi";
       networking.firewall.enable = false;
@@ -54,30 +70,10 @@
         interfaces = [ "wlan0" ];
         networks = { "yo" = { psk = "11111111"; }; };
       };
-
-      # networking.defaultGateway.address = "192.168.84.243";
-      networking.interfaces.wlan0.ipv4.addresses = [{
-        address = "192.168.143.69";
-        prefixLength = 24;
-      }];
-
-      networking.interfaces.end0.ipv4 = {
-        addresses = [
-          {
-            address = "192.168.1.100"; # Your static IP address
-            prefixLength = 24; # Netmask, 24 for 255.255.255.0
-          }
-        ];
-        routes = [
-          {
-            address = "0.0.0.0";
-            prefixLength = 0;
-            via = "192.168.1.1"; # Your gateway IP address
-          }
-        ];
-      };
-      networking.nameservers = [ "192.168.1.1" ]; # Your DNS server, often the gateway
-
+      networking.extraHosts =
+        ''
+          192.168.203.1 hytech-pi
+        '';
       systemd.services.wpa_supplicant.wantedBy =
         nixpkgs.lib.mkOverride 10 [ "default.target" ];
       # NTP time sync.
@@ -93,46 +89,73 @@
 
     can_config = {
       networking.can.enable = true;
+
       networking.can.interfaces = {
         can0 = {
           bitrate = 500000;
         };
       };
     };
-
-    pi4_config_normal = { pkgs, lib, ... }:
+    pi4_config = { pkgs, lib, ... }:
       {
-        nix.settings.require-sigs = false;
-        users.users.nixos.group = "nixos";
-        users.users.root.initialPassword = "root";
-        users.users.nixos.password = "nixos";
-        users.users.nixos.extraGroups = [ "wheel" ];
-        users.groups.nixos = { };
-        users.users.nixos.isNormalUser = true;
-
-        system.activationScripts.createRecordingsDir = lib.stringAfter [ "users" ] ''
-          mkdir -p /home/nixos/recordings
-          chown nixos:users /home/nixos/recordings
-        '';
-      };
-    nixosConfigurations.rpi4-base = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [
-        (
-          { pkgs, ... }: {
+        networking.interfaces.end0.ipv4 = {
+          addresses = [
+            {
+              address = "192.168.86.69"; # Your static IP address
+              prefixLength = 24; # Netmask, 24 for 255.255.255.0
+            }
+          ];
+          routes = [
+            {
+              address = "0.0.0.0";
+              prefixLength = 0;
+              via = "192.168.1.1"; # Your gateway IP address
+            }
+          ];
+        };
+        hardware = {
+          bluetooth.enable = true;
+          raspberry-pi = {
             config = {
-              environment.systemPackages = [
-                pkgs.can-utils
-              ];
-              sdImage.compressImage = false;
+              all = {
+                base-dt-params = {
+                  #           # enable autoprobing of bluetooth driver
+                  #           # https://github.com/raspberrypi/linux/blob/c8c99191e1419062ac8b668956d19e788865912a/arch/arm/boot/dts/overlays/README#L222-L224
+                  krnbt = {
+                    enable = true;
+                    value = "on";
+                  };
+                  spi = {
+                    enable = true;
+                    value = "on";
+                  };
+                };
+                dt-overlays = {
+                  spi-bcm2835 = {
+                    enable = true;
+                    params = { };
+                  };
+                  # TODO change this as needed
+                  mcp2515-can0 = {
+                    enable = true;
+                    params = {
+                      oscillator =
+                        {
+                          enable = true;
+                          value = "16000000";
+                        };
+                      interrupt = {
+                        enable = true;
+                        value = "16"; # this is the individual gpio number for the interrupt of the spi boi
+                      };
+                    };
+                  };
+                };
+              };
             };
-          }
-        )
-        (shared_config)
-        pi4_config_normal
-        raspberry-pi-nix.nixosModules.raspberry-pi
-      ];
-    };
+          };
+        };
+      };
     # shoutout to https://github.com/tstat/raspberry-pi-nix absolute goat
     nixosConfigurations.rpi4 = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
@@ -144,52 +167,54 @@
             config = {
               environment.systemPackages = [
                 pkgs.can-utils
+                pkgs.ethtool
+                pkgs.python3
               ];
-              sdImage.compressImage = false;
             };
             options = {
               services.data_writer.options.enable = true;
             };
+
           }
         )
         (can_config)
         (shared_config)
         raspberry-pi-nix.nixosModules.raspberry-pi
-        pi4_config_normal
-
-        ./hw/tcu_hardware.nix
+        user_setup
+        pi4_config
       ];
     };
-    nixosConfigurations.rpi3-mini-tcu = nixpkgs.lib.nixosSystem {
+
+    nixosConfigurations.rpi3 = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
       modules = [
+        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
         ./modules/data_acq.nix
-        ./modules/can_network.nix
+        ./modules/linux_router.nix
         (
           { pkgs, ... }: {
             config = {
+              sdImage.compressImage = false;
               environment.systemPackages = [
                 pkgs.can-utils
+                pkgs.ethtool
+                pkgs.python3
               ];
-              sdImage.compressImage = false;
             };
             options = {
               services.data_writer.options.enable = true;
+              services.linux_router.options.enable = true;
+              services.linux_router.options.host-ip = "192.168.203.1";
             };
+
           }
         )
-        (can_config)
+        user_setup
         (shared_config)
-        raspberry-pi-nix.nixosModules.raspberry-pi
-        pi4_config_normal
-        ./hw/mini_tcu_hardware.nix
       ];
     };
-    
-    images.rpi4-base = nixosConfigurations.rpi4-base.config.system.build.sdImage;
     images.rpi4 = nixosConfigurations.rpi4.config.system.build.sdImage;
-    images.rpi3-mini-tcu = nixosConfigurations.rpi3-mini-tcu.config.system.build.sdImage;
-
+    images.rpi3 = nixosConfigurations.rpi3.config.system.build.sdImage;
     defaultPackage.aarch64-linux = nixosConfigurations.rpi4.config.system.build.toplevel;
   };
 }
