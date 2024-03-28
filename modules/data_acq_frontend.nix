c9b634ccab3ead6c7b73ecf46fe309bbeb1f46be
Ben Hall <rcmast3r1@gmail.com>
2024-03-27 20:51:13 -0400

    yo

--- modules/data_acq_frontend.nix
+++ modules/data_acq_frontend.nix
@@ -1,10 +1,12 @@
 { config, lib, pkgs, ... }:
 with lib;
 let
+    
     cfg = config.services.data_acq_frontend;
 in {
     config = {
         systemd.services.data_acq_frontend = {
+            serviceConfig.path = [ pkgs.nodejs ];
             wantedBy = [ "multi-user.target" ];
             serviceConfig.After = [ "data_writer.service" ];
             serviceConfig.requires = [ "data_writer.service" ];
