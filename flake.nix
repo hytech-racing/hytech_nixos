088b6ee3147c2375d9fc4ccbb93bd6b6d67a2dab
Ben Hall <rcmast3r1@gmail.com>
2024-03-27 20:51:13 -0400

    yo

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/8bf65f17d8070a0a490daf5f1c784b87ee73982c";
-    hytech_data_acq.url = "github:RCMast3r/data_acq/2024-03-18T20_52_15";
+    hytech_data_acq.url = "github:RCMast3r/data_acq/frontend";
     hytech_data_acq.inputs.ht_can_pkg_flake.url = "github:hytech-racing/ht_can/31";
     raspberry-pi-nix.url = "github:tstat/raspberry-pi-nix";
 
--- modules/data_acq_frontend.nix
+++ modules/data_acq_frontend.nix
@@ -1,16 +1,17 @@
 { config, lib, pkgs, ... }:
 with lib;
 let
+    
     cfg = config.services.data_acq_frontend;
 in {
     config = {
         systemd.services.data_acq_frontend = {
-            serviceConfig.path = [pkgs.nodejs];
+            serviceConfig.path = [ pkgs.nodejs ];
             wantedBy = [ "multi-user.target" ];
             serviceConfig.After = [ "network.target" ];
             serviceConfig.requires = [ "data_writer.service" ];
-            serviceConfig.WorkingDirectory = "${pkgs.frontend_pkg}"
-            serviceConfig.ExecStart = "npm start";
-        }
-    }
+            
+            serviceConfig.ExecStart = "npm start --prefix ${pkgs.frontend_pkg.frontend}";
+        };
+    };
 }
