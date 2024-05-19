cd7aa1dff1a80c36a5ea290817467e50562c9a4b
Ben Hall <rcmast3r1@gmail.com>
2024-05-19 01:26:38 -0400

    working integration on stand

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
   inputs = rec {
     hytech_data_acq.url = "github:hytech-racing/data_acq/feature/param_server_integration";
     hytech_data_acq.inputs.ht_can_pkg_flake.url = "github:hytech-racing/ht_can/92";
-    hytech_data_acq.inputs.ht_params.url = "github:hytech-racing/HT_params/2024-05-18T02_37_57";
+    hytech_data_acq.inputs.ht_params.url = "github:hytech-racing/HT_params/2024-05-19T03_07_29";
     raspberry-pi-nix.url = "github:tstat/raspberry-pi-nix/b39b556e8a1c4bd6f8a59e8719dc1f658f18f255";
     nixpkgs.url = "github:NixOS/nixpkgs/8bf65f17d8070a0a490daf5f1c784b87ee73982c";
 
