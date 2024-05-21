3924834ced6a78e5a8399ab8d61bc66ff1e607b0
Christine <132790780+aesteri@users.noreply.github.com>
2024-05-20 23:17:54 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   };
 
   inputs = rec {
-    hytech_data_acq.url = "github:hytech-racing/data_acq/feature/param_server_integration";
+    hytech_data_acq.url = "github:hytech-racing/data_acq/video_integration";
     hytech_data_acq.inputs.ht_can_pkg_flake.url = "github:hytech-racing/ht_can/92";
     hytech_data_acq.inputs.ht_params.url = "github:hytech-racing/HT_params/2024-05-19T03_07_29";
     raspberry-pi-nix.url = "github:tstat/raspberry-pi-nix/b39b556e8a1c4bd6f8a59e8719dc1f658f18f255";
