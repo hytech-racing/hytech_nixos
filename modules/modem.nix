574a18710b4a058bbe1af0b3d5091b40558e69b1
Christine Kim <132790780+aesteri@users.noreply.github.com>
2024-06-05 13:57:17 -0400

    Update modem.nix

--- modules/modem.nix
+++ modules/modem.nix
@@ -11,7 +11,8 @@ in
       description = "Modem Network Interfaces Setup";
       wantedBy = [ "multi-user.target" ];
       serviceConfig.After = [ "network.target" ];
-      script = ''
+      ExecStartPre=timeout 60s bash -c 'until ip a s wwu1i4; do sleep 1; done'
+      ExecStart = ''
             sudo ip link set wwu1i4 down
             echo 'Y' | sudo tee /sys/class/net/wwu1i4/qmi/raw_ip
             sudo ip link set wwu1i4 up
