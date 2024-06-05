0b0bd7c8dd6ae36d324757d26a3fc13d45e27d9b
Christine <132790780+aesteri@users.noreply.github.com>
2024-06-05 02:12:18 -0400

    d

--- modules/data_acq_frontend.nix
+++ modules/data_acq_frontend.nix
@@ -9,14 +9,6 @@ in {
             path = [ pkgs.nodejs pkgs.bash pkgs.nodePackages.serve pkgs.getconf ];
             enable = true;
             wantedBy = [ "default.target" ];
-            activationScripts =
-            ''
-                sudo ip link set wwu1i4 down
-                echo 'Y' | sudo tee /sys/class/net/wwu1i4/qmi/raw_ip
-                sudo ip link set wwu1i4 up
-                sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='fast.t-mobile.com',ip-type=4" --client-no-release-cid
-                sudo udhcpc -q -f -i wwu1i4
-            '';
             serviceConfig.After = [ "network.target" ];
             serviceConfig.ExecStart = "${pkgs.nodePackages.serve}/bin/serve ${pkgs.frontend_pkg.frontend}/build -l 4000";
         };
--- modules/simple_http_server.nix
+++ modules/simple_http_server.nix
@@ -14,6 +14,14 @@ in
     systemd.services.http_server = {
       wantedBy = [ "multi-user.target" ];
       description = "Allows you to access your MCAP files";
+      activationScripts =
+            ''
+                sudo ip link set wwu1i4 down
+                echo 'Y' | sudo tee /sys/class/net/wwu1i4/qmi/raw_ip
+                sudo ip link set wwu1i4 up
+                sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='fast.t-mobile.com',ip-type=4" --client-no-release-cid
+                sudo udhcpc -q -f -i wwu1i4
+            '';
       serviceConfig.ExecStart = "${pkgs.simple-http-server}/bin/simple-http-server --port ${escapeShellArg cfg.port} /home/nixos/recordings";
       serviceConfig.ExecStop = "/bin/kill -SIGINT $MAINPID";
       serviceConfig.Restart = "on-failure";
