4e1836a79b153688c6677978619671e8f273e791
Christine <132790780+aesteri@users.noreply.github.com>
2024-06-05 00:56:03 -0400

    testing

---
+++ modules/modem.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+
+    cfg = config.services.services.modem;
+in {
+    config = {
+        systemd.services.modem = {
+          script =
+          ''
+            sudo ip link set wwu1i4 down
+            echo 'Y' | sudo tee /sys/class/net/wwu1i4/qmi/raw_ip
+            sudo ip link set wwu1i4 up
+            sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='fast.t-mobile.com',ip-type=4" --client-no-release-cid
+            sudo udhcpc -q -f -i wwu1i4
+          '';
+          };
+    };
+}
