97895067c4993807279974f3bb4132f70e3021d3
Christine <132790780+aesteri@users.noreply.github.com>
2024-06-05 02:57:19 -0400

    Update modem.nix

--- modules/modem.nix
+++ modules/modem.nix
@@ -12,14 +12,13 @@ in
       wantedBy = [ "multi-user.target" ];
       serviceConfig.After = [ "network.target" ];
       script = ''
-            sudo ip link set wwu1i4 down
-            echo 'Y' | sudo tee /sys/class/net/wwu1i4/qmi/raw_ip
-            sudo ip link set wwu1i4 up
+            sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='fast.t-mobile.com',ip-type=4" --client-no-release-cid
             sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='fast.t-mobile.com',ip-type=4" --client-no-release-cid
             sudo udhcpc -q -f -i wwu1i4
         '';
       reload = ''
           sudo ip link set wwu1i4 down
+          sudo ip link set wwu1i4 up
         '';
     };
   };
