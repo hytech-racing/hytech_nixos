0ff09de7e3d1754fc53e99e63134b0106494b42f
Christine <132790780+aesteri@users.noreply.github.com>
2024-06-05 02:35:53 -0400

    Update modem.nix

--- modules/modem.nix
+++ modules/modem.nix
@@ -14,17 +14,17 @@ in
       before = [ "network.target" ];
       script = iface: ''
           if ! ip link show wwu1i4 | grep -q "UP"; then
-            ${ipCmd} ip link set wwu1i4 down
+            sudo ip link set wwu1i4 down
             echo 'Y' | sudo tee /sys/class/net/wwu1i4/qmi/raw_ip
-            ${ipCmd} ip link set wwu1i4 up
-            ${ipCmd} qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='fast.t-mobile.com',ip-type=4" --client-no-release-cid
-            ${ipCmd} udhcpc -q -f -i wwu1i4
+            sudo ip link set wwu1i4 up
+            sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='fast.t-mobile.com',ip-type=4" --client-no-release-cid
+            sudo udhcpc -q -f -i wwu1i4
           else
             echo "wwu1i4 is already up."
           fi
         '';
       reload = iface: ''
-          ${ipCmd} ip link set wwu1i4 down
+          sudo ip link set wwu1i4 down
         '';
       path = [ pkgs.iproute2 ];
     };
