c7d724da8068e740be89074063d291a96111052e
Christine <132790780+aesteri@users.noreply.github.com>
2024-06-05 00:37:09 -0400

    Update standard_settings.nix

--- modules/shared_config/standard_settings.nix
+++ modules/shared_config/standard_settings.nix
@@ -15,12 +15,6 @@
     system.activationScripts.createRecordingsDir = lib.stringAfter [ "users" ] ''
       mkdir -p /home/nixos/recordings
       chown nixos:users /home/nixos/recordings
-      sudo ip link set wwu1i4 down
-      echo 'Y' | sudo tee /sys/class/net/wwu1i4/qmi/raw_ip
-      sudo ip link set wwu1i4 up
-      sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='fast.t-mobile.com',ip-type=4" --client-no-release-cid
-      sudo udhcpc -q -f -i wwu1i4
-      
     '';
     users.extraUsers.nixos.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSt9Z8Qdq068xj/ILVAMqmkVyUvKCSTsdaoehEZWRut rcmast3r1@gmail.com"
