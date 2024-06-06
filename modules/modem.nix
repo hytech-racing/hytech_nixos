1a98f3b2d8ec9f713e20d4ea8fc040a1408ebf06
Christine Kim <132790780+aesteri@users.noreply.github.com>
2024-06-05 20:45:00 -0400

    Update modem.nix

--- modules/modem.nix
+++ modules/modem.nix
@@ -3,27 +3,66 @@
 with lib;
 
 let
-  cfg = config.services.modem;
+  cfg = config.networking.modem;
+  ipCmd = "${pkgs.iproute2}/bin/ip";
+  qmiCmd = "${pkgs.libqmi}/bin/qmicli";
+  udhcpcCmd = "${pkgs.busybox}/bin/udhcpc";
 in
 {
-  config = {
+  options.networking.modem = {
+    enable = mkEnableOption "Modem network interfaces";
 
-    systemd.services.modem = {
+    interfaces = mkOption {
+      default = { };
+      example = literalExpression ''{
+        wwu1i4 = {
+          apn = "fast.t-mobile.com";
+        };
+      }'';
+      type = types.attrsOf (types.submodule {
+        options = {
+          name = mkOption {
+            type = types.str;
+            description = "The name of the modem interface.";
+          };
+          apn = mkOption {
+            type = types.str;
+            description = "The APN of the modem interface.";
+          };
+        };
+      });
+      description = "Modem interface configurations.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      iproute2
+      libqmi
+      busybox
+    ];
+
+    systemd.services.modem-setup = {
       description = "Modem Network Interfaces Setup";
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
-      path = [ pkgs.libqmi pkgs.busybox pkgs.sudo ];  # Ensure qmicli and udhcpc are in the PATH
+      path = [ pkgs.iproute2 pkgs.libqmi pkgs.busybox ];
 
-      serviceConfig.ExecStart = ''
-        sudo ip link set wwu1i4 down
-        echo 'Y' > /sys/class/net/wwu1i4/qmi/raw_ip
-        sudo ip link set wwu1i4 up
-        sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='fast.t-mobile.com',ip-type=4" --client-no-release-cid
-        sudo udhcpc -q -f -i wwu1i4
-      '';
+      serviceConfig = {
+        ExecStart = concatStringsSep "\n" (mapAttrsToList
+          (name: iface: ''
+            ${ipCmd} link set ${name} down
+            echo 'Y' > /sys/class/net/${name}/qmi/raw_ip
+            ${ipCmd} link set ${name} up
+            ${qmiCmd} -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='${iface.apn}',ip-type=4" --client-no-release-cid
+            ${udhcpcCmd} -q -f -i ${name}
+          '')
+          cfg.interfaces);
+        Restart = "always";
+      };
     };
 
-    system.activationScripts.setupwwu1i4 = lib.mkAfter '' 
+    system.activationScripts.setupModemInterfaces = lib.mkAfter '' 
       mkdir -p /etc/network/interfaces.d
       cat << EOF > /etc/network/interfaces.d/wwu1i4
 auto wwu1i4
