e194b544e2687f89085ab1279babda5d9779e97d
Christine Kim <132790780+aesteri@users.noreply.github.com>
2024-06-05 20:33:02 -0400

    Update standard_settings.nix

--- modules/shared_config/standard_settings.nix
+++ modules/shared_config/standard_settings.nix
@@ -1,5 +1,5 @@
 # contains the shared configuration for nix, the nixos user on tcu, standard programs
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 {
   config = {
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -16,21 +16,6 @@
       mkdir -p /home/nixos/recordings
       chown nixos:users /home/nixos/recordings
     '';
-     system.activationScripts.setupWwu1i4 = lib.mkAfter '' 
-            mkdir -p /etc/network/interfaces.d
-            cat << EOF > /etc/network/interfaces.d/wwu1i4
-      auto wwu1i4
-      iface wwu1i4 inet manual
-           pre-up ip link set wwu1i4 down
-           pre-up echo Y > /sys/class/net/wwu1i4/qmi/raw_ip
-           pre-up for _ in \$(seq 1 10); do /usr/bin/test -c /dev/cdc-wdm0 && break; /bin/sleep 1; done
-           pre-up for _ in \$(seq 1 10); do /usr/bin/qmicli -d /dev/cdc-wdm0 --nas-get-signal-strength && break; /bin/sleep 1; done
-           pre-up qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='fast.t-mobile.com',ip-type=4" --client-no-release-cid
-           pre-up udhcpc -i wwu1i4
-           post-down /usr/bin/qmi-network /dev/cdc-wdm0 stop
-      EOF
-            chmod 644 /etc/network/interfaces.d/wwu1i4
-          '';
 
     users.extraUsers.nixos.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSt9Z8Qdq068xj/ILVAMqmkVyUvKCSTsdaoehEZWRut rcmast3r1@gmail.com"
