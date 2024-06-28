d691affbe64c750874f4c79efb00ccaaa4bfb16a
Christine Kim <132790780+aesteri@users.noreply.github.com>
2024-06-28 18:20:46 -0400

    Update modem.nix

--- modules/modem.nix
+++ modules/modem.nix
@@ -10,13 +10,18 @@ in
     systemd.services.modem = {
       wantedBy = [ "multi-user.target" ];
       serviceConfig.After = [ "network.target" "ttyUSB2.device" "cdc-wdm0" ];
-      script = ''
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = ''
           sudo ip link set wwu1i4 down
-          echo 'Y' | sudo tee /sys/class/net/wwu1i4/qmi/raw_ip
+          sudo echo 'Y' | sudo tee /sys/class/net/wwu1i4/qmi/raw_ip
           sudo ip link set wwu1i4 up
           sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='fast.t-mobile.com',ip-type=4" --client-no-release-cid
           sudo udhcpc -q -f -i wwu1i4
-      '';
+        '';
+        StandardOutput = "journal";
+        StandardError = "journal";
+      };
     };
   };
 }
