1a063dadea26b9ff66448d0049d8beba8d3f2fce
Christine Kim <132790780+aesteri@users.noreply.github.com>
2024-06-28 17:35:21 -0400

    Update modem.nix

--- modules/modem.nix
+++ modules/modem.nix
@@ -15,7 +15,6 @@ in
           echo 'Y' | sudo tee /sys/class/net/wwu1i4/qmi/raw_ip
           sudo ip link set wwu1i4 up
           sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='fast.t-mobile.com',ip-type=4" --client-no-release-cid
-          sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='fast.t-mobile.com',ip-type=4" --client-no-release-cid
           sudo udhcpc -q -f -i wwu1i4
       '';
     };
