01138a72e1db4096f621a55d9a0afef0981b5f97
Christine Kim <132790780+aesteri@users.noreply.github.com>
2024-07-18 17:21:50 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   };
 
   inputs = rec {
-    hytech_data_acq.url = "github:hytech-racing/data_acq/2024-04-27T00_26_50";
+    hytech_data_acq.url = "github:hytech-racing/data_acq/video_sqaureone";
     hytech_data_acq.inputs.ht_can_pkg_flake.url = "github:hytech-racing/ht_can/102";
     aero_sensor_logger.url = "github:hytech-racing/aero_sensor_logger/8ff36ab9256d6f22ad04aff68c3fabc5f2de796d";
     hytech_params_server.url = "github:hytech-racing/HT_params/2024-05-26T15_33_34";
@@ -77,6 +77,7 @@
               services.linux_router.host-ip = "192.168.203.1";
               services.http_server.port = 8001;
               services.param_webserver.enable = true;
+              raspberry-pi-nix.libcamera-overlay.enable = false;
               # aero-sensor-logger.enable = true;
               # service_names.url-name = ".car";
               # service_names.car-ip = "192.168.1.69";
