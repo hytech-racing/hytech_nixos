0c23384b1fdfcf4f3ef6584a95c236f90f2458d7
Christine Kim <132790780+aesteri@users.noreply.github.com>
2024-08-15 06:12:02 -0400

    Update standard_services.nix

--- modules/shared_config/standard_services.nix
+++ modules/shared_config/standard_services.nix
@@ -48,10 +48,6 @@ in
         port = 22;
       }
     ];
-    services.tailscale = {
-        enable = true;
-        useRoutingFeatures = if cfg.advertiseExitNode then "server" else "client";
-      };
     # create a oneshot job to authenticate to Tailscale
     systemd.services.tailscale-autoconnect = {
       description = "Automatic connection to Tailscale";
