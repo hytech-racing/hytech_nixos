7706c565cc49c25c0c06d6e24d9c52df0a73ccbe
nebudev14 <wyun2006@gmail.com>
2025-09-24 18:27:18 -0400

    canbus logging service

--- flake.nix
+++ flake.nix
@@ -1,8 +1,14 @@
 {
   description = "Build image";
   nixConfig = {
-    extra-substituters = [ "https://nix-community.cachix.org" "https://rcmast3r.cachix.org" ];
-    extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "rcmast3r.cachix.org-1:dH22dF877RZ1j7uvAgqnQWNChxdQDeqgBRWpXzoi84c=" ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://rcmast3r.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "rcmast3r.cachix.org-1:dH22dF877RZ1j7uvAgqnQWNChxdQDeqgBRWpXzoi84c="
+    ];
   };
 
   inputs = rec {
@@ -12,7 +18,7 @@
       url = "https://github.com/hytech-racing/drivebrain_simulink_models/releases/download/rel13-2/gen_rel.tar.gz";
       flake = false;
     };
-    
+
     drivebrain-software.url = "github:hytech-racing/drivebrain_software/dev/v1.1.0";
     drivebrain-software.inputs.ht_can.follows = "ht_can";
     drivebrain-software.inputs.HT_proto.follows = "ht_proto";
@@ -24,7 +30,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/cbd8ec4de4469333c82ff40d057350c30e9f7d36";
     # nixpkgs.follows = "raspberry-pi-nix/nixpkgs";
 
-
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
@@ -34,149 +39,175 @@
     nixos-shell.url = "github:Mic92/nixos-shell";
   };
 
-  outputs = { self, nixpkgs, raspberry-pi-nix, nixos-generators, home-manager, drivebrain-software, ... }@inputs: rec {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      raspberry-pi-nix,
+      nixos-generators,
+      home-manager,
+      drivebrain-software,
+      ...
+    }@inputs:
+    rec {
 
-    nixpkg_overlays =
-      {
-        nixpkgs.overlays =
-          [
-            (self: super: {
-              linux-router = super.linux-router.override {
-                useQrencode = false;
-              };
-            })
-            drivebrain-software.overlays.default
-            drivebrain-software.inputs.easy_cmake.overlays.default
-            drivebrain-software.inputs.nebs-packages.overlays.default
-            drivebrain-software.inputs.vn_driver_lib.overlays.default
-            drivebrain-software.inputs.ht_can.overlays.default
+      nixpkg_overlays = {
+        nixpkgs.overlays = [
+          (self: super: {
+            linux-router = super.linux-router.override {
+              useQrencode = false;
+            };
+          })
+          drivebrain-software.overlays.default
+          drivebrain-software.inputs.easy_cmake.overlays.default
+          drivebrain-software.inputs.nebs-packages.overlays.default
+          drivebrain-software.inputs.vn_driver_lib.overlays.default
+          drivebrain-software.inputs.ht_can.overlays.default
 
+        ];
+      };
+
+      shared_config_modules = [
+        ./modules/shared_config/environment.nix
+        ./modules/shared_config/networking.nix
+        ./modules/shared_config/standard_settings.nix
+        ./modules/shared_config/standard_services.nix
+        ./modules/software_config/drivebrain_software.nix
+      ];
+
+      hw_config_modules = [
+        ./modules/linux_router.nix
+        ./modules/hardware_config/hw_config.nix
+      ];
+
+      hytech_service_modules = [
+        ./modules/can_network.nix
+        ./modules/simple_http_server.nix
+        ./modules/grpcui.nix
+        ./modules/logging_config/logging.nix
+      ];
+
+      nixosConfigurations.tcu = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+
+        specialArgs = { inherit self; };
+        modules =
+          hw_config_modules
+          ++ hytech_service_modules
+          ++ shared_config_modules
+          ++ [
+            (nixpkg_overlays)
+            home-manager.nixosModules.home-manager
+            inputs.raspberry-pi-nix.nixosModules.raspberry-pi
+            inputs.raspberry-pi-nix.nixosModules.sd-image
+            (
+              { config, options, ... }:
+              rec {
+                nixpkgs.hostPlatform.system = "aarch64-linux";
+
+                services.linux_router.host-ip = "192.168.203.1";
+                services.http_server.port = 8001;
+                drivebrain-service.enable = true;
+                simple_http_server.enable = true;
+                services.grpcui.enable = true;
+                tcu_config.enable = true;
+                hytech-nixos-environment.enable = true;
+                hytech-nixos-networking.enable = true;
+                standard-services.enable = true;
+                standard-settings.enable = true;
+                linux_router.enable = true;
+
+                raspberry-pi-nix.libcamera-overlay.enable = false;
+                raspberry-pi-nix.board = "bcm2712";
+                # raspberry-pi-nix.kernel-version = "v6_12_17";
+
+              }
+            )
           ];
       };
 
-    shared_config_modules = [
-      ./modules/shared_config/environment.nix
-      ./modules/shared_config/networking.nix
-      ./modules/shared_config/standard_settings.nix
-      ./modules/shared_config/standard_services.nix
-      ./modules/software_config/drivebrain_software.nix
-    ];
-
-    hw_config_modules = [
-      ./modules/linux_router.nix
-      ./modules/hardware_config/hw_config.nix
-    ];
-
-    hytech_service_modules = [
-      ./modules/can_network.nix
-      ./modules/simple_http_server.nix
-      ./modules/grpcui.nix
-    ];
-
-    nixosConfigurations.tcu = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-
-      specialArgs = { inherit self; };
-      modules =
-        hw_config_modules ++
-        hytech_service_modules ++
-        shared_config_modules ++ [
-          (nixpkg_overlays)
-          home-manager.nixosModules.home-manager
-          inputs.raspberry-pi-nix.nixosModules.raspberry-pi
-          inputs.raspberry-pi-nix.nixosModules.sd-image
-          (
-            { config, options, ... }: rec {
-              nixpkgs.hostPlatform.system = "aarch64-linux";
-
-              services.linux_router.host-ip = "192.168.203.1";
-              services.http_server.port = 8001;
-              drivebrain-service.enable = true;
-              simple_http_server.enable = true;
-              services.grpcui.enable = true;
-              tcu_config.enable = true;
-              hytech-nixos-environment.enable = true;
-              hytech-nixos-networking.enable = true;
-              standard-services.enable = true;
-              standard-settings.enable = true;
-              linux_router.enable = true;
-
-              raspberry-pi-nix.libcamera-overlay.enable = false;
-              raspberry-pi-nix.board = "bcm2712";
-              # raspberry-pi-nix.kernel-version = "v6_12_17";
-
-            }
-          )
-        ];
-    };
-
-    test-shell = {
-      system = "x86_64-linux";
-      specialArgs = { inherit self; };
-      modules =
-        shared_config_modules ++
-        hytech_service_modules ++
-        [
-          inputs.nixos-shell.nixosModules.nixos-shell
-          (nixpkg_overlays)
-          (
-            { config, ... }: {
-              virtualisation.forwardPorts = [
-                { from = "host"; host.port = 22000; guest.port = 22; }
-                { from = "host"; host.port = 8001; guest.port = 8001; }
-                { from = "host"; host.port = 8000; guest.port = 80; }
-              ];
-              networking.can.enable = true;
-              networking.can.interfaces = {
-                vcan0 = { # kvaser usb CAN
-                  enable = true;
-                };
-                vcan1 = { # aux SPI CAN
-                  enable = true;
-                };
-              };
-              networking.interfaces.eth0.ipv4 = {
-                addresses = [
+      test-shell = {
+        system = "x86_64-linux";
+        specialArgs = { inherit self; };
+        modules =
+          shared_config_modules
+          ++ hytech_service_modules
+          ++ [
+            inputs.nixos-shell.nixosModules.nixos-shell
+            (nixpkg_overlays)
+            (
+              { config, ... }:
+              {
+                virtualisation.forwardPorts = [
                   {
-                    address = "192.168.1.30"; # Your static IP address
-                    prefixLength = 24; # Netmask, 24 for 255.255.255.0
+                    from = "host";
+                    host.port = 22000;
+                    guest.port = 22;
+                  }
+                  {
+                    from = "host";
+                    host.port = 8001;
+                    guest.port = 8001;
+                  }
+                  {
+                    from = "host";
+                    host.port = 8000;
+                    guest.port = 80;
                   }
                 ];
-                routes = [
-                  {
-                    address = "0.0.0.0";
-                    prefixLength = 0;
-                    via = "192.168.1.1"; # Your gateway IP address
-                  }
-                ];
-              };
-              services.http_server.port = 8001;
-              drivebrain-service.enable = true;
-              simple_http_server.enable = true;
-              services.grpcui.enable = true;
-              hytech-nixos-environment.enable = true;
-              hytech-nixos-networking.enable = true;
-              standard-services.enable = true;
-              standard-settings.enable = true;
-            }
-          )
-        ];
+                networking.can.enable = true;
+                networking.can.interfaces = {
+                  vcan0 = {
+                    # kvaser usb CAN
+                    enable = true;
+                  };
+                  vcan1 = {
+                    # aux SPI CAN
+                    enable = true;
+                  };
+                };
+                networking.interfaces.eth0.ipv4 = {
+                  addresses = [
+                    {
+                      address = "192.168.1.30"; # Your static IP address
+                      prefixLength = 24; # Netmask, 24 for 255.255.255.0
+                    }
+                  ];
+                  routes = [
+                    {
+                      address = "0.0.0.0";
+                      prefixLength = 0;
+                      via = "192.168.1.1"; # Your gateway IP address
+                    }
+                  ];
+                };
+                services.http_server.port = 8001;
+                drivebrain-service.enable = true;
+                simple_http_server.enable = true;
+                services.grpcui.enable = true;
+                hytech-nixos-environment.enable = true;
+                hytech-nixos-networking.enable = true;
+                standard-services.enable = true;
+                standard-settings.enable = true;
+              }
+            )
+          ];
+      };
+
+      packages = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = nixpkg_overlays.nixpkgs.overlays;
+
+      };
+      # virtualbox_vm_modules = { modules = support_vm_config.modules ++ [ ./modules/vm_config/virtualbox_config.nix ]; };
+      # cc_vm_modules = { modules = support_vm_config.modules ++ [ ./modules/vm_config/basic_vm_hw.nix ]; };
+      # Use nixos-generate to create the VMs
+      # nixosConfigurations.vbi = nixos-generators.nixosGenerate (support_vm_config // virtualbox_vm_modules // { format = "virtualbox"; });
+      nixosConfigurations.test-shell = nixpkgs.lib.nixosSystem (test-shell);
+
+      images.tcu = nixosConfigurations.tcu.config.system.build.sdImage;
+      tcu_top = nixosConfigurations.tcu.config.system.build.toplevel;
+      config_test = nixosConfigurations.tcu.config.hardware.raspberry-pi.config-output;
+      shell = nixosConfigurations.test-shell.config.system.build.nixos-shell;
     };
-
-    packages = import nixpkgs {
-      system = "x86_64-linux";
-      overlays = nixpkg_overlays.nixpkgs.overlays;
-
-    };
-    # virtualbox_vm_modules = { modules = support_vm_config.modules ++ [ ./modules/vm_config/virtualbox_config.nix ]; };
-    # cc_vm_modules = { modules = support_vm_config.modules ++ [ ./modules/vm_config/basic_vm_hw.nix ]; };
-    # Use nixos-generate to create the VMs
-    # nixosConfigurations.vbi = nixos-generators.nixosGenerate (support_vm_config // virtualbox_vm_modules // { format = "virtualbox"; });
-    nixosConfigurations.test-shell = nixpkgs.lib.nixosSystem (test-shell);
-
-    images.tcu = nixosConfigurations.tcu.config.system.build.sdImage;
-    tcu_top = nixosConfigurations.tcu.config.system.build.toplevel;
-    config_test = nixosConfigurations.tcu.config.hardware.raspberry-pi.config-output;
-    shell = nixosConfigurations.test-shell.config.system.build.nixos-shell;
-  };
 }
---
+++ modules/logging_config/log.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# TODO: idk what the channel names are
+CHANNELS=(can_primary can_secondary)
+declare -A PIDS
+CURR_MP=""
+
+timestamp() {
+  awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0; fflush(); }'
+}
+
+wait_for_mount() {
+  local devnode="$1" mp=""
+  while :; do
+    mp="$(lsblk -no MOUNTPOINT "$devnode" | grep -m1 . || true)"
+    [[ -n "$mp" ]] && { echo "$mp"; return; }
+    sleep 1
+  done
+}
+
+init_dump() {
+  CURR_MP="$1"
+  for ch in "${CHANNELS[@]}"; do
+    local dir="$CURR_MP/$ch"
+    mkdir -p "$dir"
+    local file="$dir/$(date +"%Y-%m-%d_%H-%M-%S.log")"
+    echo "[can-log] starting candump on $ch into $dir/"
+    # (candump -l "$ch" | timestamp>"$dir/")
+
+    ( yes "$ch dummy log line" | timestamp>"$file" ) &
+    PIDS[$ch]=$!
+  done
+}
+
+stop_dump() {
+  echo "[can-log] stopping candump process"
+  for ch in "${CHANNELS[@]}"; do
+    if [[ -n "${PIDS[$ch]:-}" ]]; then
+      kill "${PIDS[$ch]}" 2>/dev/null || true
+      unset PIDS[$ch]
+    fi
+  done
+  CURR_MP=""
+}
+
+echo "[can-log] waiting for usb disk events"
+
+udevadm monitor --udev --subsystem-match=block --property | \
+  awk '
+    BEGIN { action=""; devname=""; devtype=""; idbus=""; }
+    NF==0 {
+      if (idbus=="usb" && devtype=="disk" && devname ~ /^\/dev\/sd[a-z]+$/) {
+        print action, devname; fflush(stdout);
+      }
+      action=""; devname=""; devtype=""; idbus="";
+      next
+    }
+    /^ACTION=/  { sub(/^ACTION=/,""); action=$0; next }
+    /^DEVNAME=/ { sub(/^DEVNAME=/,""); devname=$0; next }
+    /^DEVTYPE=/ { sub(/^DEVTYPE=/,""); devtype=$0; next }
+    /^ID_BUS=/  { sub(/^ID_BUS=/,""); idbus=$0; next }
+    ' | while read -r ACTION DEVNODE; do
+      case "$ACTION" in
+        add)
+          echo "[can-log] detected $DEVNODE, awaiting mount"
+          MP="$(wait_for_mount "$DEVNODE")"
+          echo "[can-log] mounted at $MP"
+          init_dump "$MP"
+          ;;
+        remove)
+          echo "[can-log] device $DEVNODE removed"
+          stop_dump
+          ;;
+      esac
+    done
+
