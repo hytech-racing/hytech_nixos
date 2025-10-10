66dbca775122c5d873c1786ed60c07be4d0e9742
nebudev14 <wyun2006@gmail.com>
2025-10-09 23:39:16 -0400

    Fix log perms

--- modules/logging_config/logging.nix
+++ modules/logging_config/logging.nix
@@ -12,20 +12,37 @@
   };
 
   config = lib.mkIf config.logging-service.enable {
+
     systemd.services.logging-service = {
       description = "CAN Logging Service";
       wantedBy = [ "multi-user.target" ];
-      before = [ "network.target" ];
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
 
       serviceConfig = {
         Type = "simple";
         ExecStart = "${pkgs.bash}/bin/bash ${./log.sh}";
         Restart = "on-failure";
         RestartSec = 2;
+        User = "root";
+        WorkingDirectory = "/mnt";
         StandardOutput = "journal";
         StandardError = "journal";
-        Environment = "PATH=${lib.makeBinPath [ pkgs.bash pkgs.can-utils pkgs.coreutils pkgs.gnugrep ]}";
+        CapabilityBoundingSet = "CAP_NET_ADMIN CAP_NET_RAW";
+        AmbientCapabilities = "CAP_NET_ADMIN CAP_NET_RAW";
+        NoNewPrivileges = false;
+        RequiresMountsFor = [ "/mnt" ];
+        Environment = "PATH=${
+          lib.makeBinPath [
+            pkgs.bash
+            pkgs.can-utils
+            pkgs.coreutils
+            pkgs.gnugrep
+            pkgs.iproute2
+          ]
+        }";
       };
+
     };
   };
 }
